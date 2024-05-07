2b65a03de4ff81c322cfc7220313b95163b208d6
dungeonmaster161 <uttam.shikhar16@gmail.com>
2024-05-07 19:52:04 +0530

    todo app is addded for hooks

---
+++ Mix/src/hooks/HookUseEffect.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+/*
+UseEffect hook is use to perfom side effect in function components . 
+Side effect are opration that can affect other component or cant be done during 
+rendering , such as data fetching , subscriptions , or manually changing the dom in 
+react components
+
+The useEffect hook serves the same purpose as 'componentDidMount', 'componentDidUpdate' and
+'componentWillMount' in react class components but inified into single api
+
+
+useEffect(functon(){},[]) [] means it only runs once when components renders
+[] takes state variable as input any time [count] this count changes this useEffect will be called
+*/
+
+export default function HookUseState() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ todo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState(0)
+  
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
