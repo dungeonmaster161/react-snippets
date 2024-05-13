dbecce10e419c408bc6921b996af3833f918d0e8
dungeonmaster161 <uttam.shikhar16@gmail.com>
2024-05-13 23:17:03 +0530

    react snippets are updated

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 
 function App() {
   const [todo, setTodo] = useState(0)
-  
+  useEffect(()=>{
+    fetch("https://sum-server.100xdevs.com/todos")
+    .then(function(res){
+      // const json = await res.json();
+    })
+  },[])
   return (
     <>
-     
+    will work on this compnent
     </>
   )
 }
